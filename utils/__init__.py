7ad39117e69d1fa52c1a70b3f72f66ec80d63438
Javier Castellanos <javierbq@gmail.com>
2014-06-13 01:16:15 -0700

    Fixed command line execution of mstate_contact_opt.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -22,18 +22,18 @@ def get_cmd_from_yaml(file, input_dir):
     return cmd
 
 
-def run_target(target_dir, benchid, input_dir, redis_host_port, session, dummy_run=False):
+def run_target(target_dir, benchid, input_dir, redis_host_port, session, env_path, dummy_run=False):
     target = os.path.basename(target_dir)
     logger.info('Running target %s' % target)
     session.sendline('cd %s' % target_dir)
-    session.sync_original_prompt()
     cmd = get_cmd_from_yaml('%s/target.yaml' % target_dir, input_dir) + ' -redis_host_port %s %d -job_name %s' % (redis_host_port[0], redis_host_port[1], '%s_%s' % (benchid, target))
     logger.debug('cmd: %s' % cmd)
     if dummy_run:
       logger.info('dummy run! skipping execution.')
-      #session.sendline('bash -c "pwd > ~/log.txt"')
-      logger.debug('remote pwd:' + session.before)
     else:
-      session.sendline('echo "test" &>> ~/stdout.log')
-      logger.debug('response:' + session.before)
+      logger.debug('Executing command on head node.')
+      #import ipdb
+      #ipdb.set_trace()
+      session.sendline('nohup ' + env_path + '/bin/' + cmd + ' &')
+      session.prompt()
     logger.info('done!.')
